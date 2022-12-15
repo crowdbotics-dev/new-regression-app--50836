051d3f167fce4ca846930934aecfd2c136d6e54d
Ravinder sales <ravinder.y@crowdbotics.com>
2022-12-15 18:38:32 +0530

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "new-regression-app--50836",
   url: "https://new-regression-app--50836.botics.co",
   api: "https://new-regression-app--50836.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
